3eabe53e57a3fbda83301181bd1dc4636c548e2f
root <root@localhost.localdomain>
2018-03-06 18:44:56 +0800

    make.sh

--- nginx-source-comment/nginx-1.10.3/make.sh
+++ nginx-source-comment/nginx-1.10.3/make.sh
@@ -1,4 +1,4 @@
 make clean
 ./configure --with-debug --without-http_ssi_module  --without-http_userid_module --without-http_auth_basic_module --without-http_split_clients_module --without-http_uwsgi_module --without-http_scgi_module --without-mail_pop3_module --without-mail_imap_module --without-mail_smtp_module
 sed -i s/-O// objs/Makefile
-make -j8
+make
