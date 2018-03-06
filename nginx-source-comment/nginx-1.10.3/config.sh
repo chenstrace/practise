6466763a93e41139d0bfb479e7377185fc73b2c4
root <root@localhost.localdomain>
2018-03-06 18:08:53 +0800

    config.sh

---
+++ nginx-source-comment/nginx-1.10.3/config.sh
@@ -0,0 +1,1 @@
+./configure --with-debug --without-http_ssi_module  --without-http_userid_module --without-http_auth_basic_module --without-http_split_clients_module --without-http_uwsgi_module --without-http_scgi_module --without-mail_pop3_module --without-mail_imap_module --without-mail_smtp_module
