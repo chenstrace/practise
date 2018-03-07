79c01a725020985a072768d8815172723e047fa9
root <root@localhost.localdomain>
2018-03-07 17:00:35 +0800

    ggdb3

--- nginx-source-comment/nginx-1.10.3/make.sh
+++ nginx-source-comment/nginx-1.10.3/make.sh
@@ -1,4 +1,6 @@
 make clean
 ./configure --with-debug --without-http_ssi_module  --without-http_userid_module --without-http_auth_basic_module --without-http_split_clients_module --without-http_uwsgi_module --without-http_scgi_module --without-mail_pop3_module --without-mail_imap_module --without-mail_smtp_module
-sed -i s/-O// objs/Makefile
+line_number=`sed -n '/CFLAGS/=' objs/Makefile |head -n1`
+sed -i "${line_number}d" objs/Makefile
+sed -i "${line_number} aCFLAGS = -pipe -W -Wall -Wpointer-arith -Wno-unused-parameter -Werror -ggdb3" objs/Makefile
 make -j8
