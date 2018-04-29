dcd318dd77ebf51dfcb342164142b5791697eb85
chenstrace <chenstrace@163.com>
2018-04-29 21:10:24 +0800

    不同的模块， 可以存在名称一样的指令。 比如listen指令，同时存在于ngx_http_core_module、ngx_mail_core_module、ngx_stream_core_module这三个模块中

--- nginx-source-comment/nginx-1.10.3/src/core/ngx_conf_file.c
+++ nginx-source-comment/nginx-1.10.3/src/core/ngx_conf_file.c
@@ -328,7 +328,7 @@ ngx_conf_handler(ngx_conf_t *cf, ngx_int_t last)
     name = cf->args->elts;
 
     found = 0;
-    /*对各个模块的各个指令进行相应的设置操作*/
+    /*对各个模块的各个指令进行相应的设置操作,从这里可以看出，不同的模块， 可以存在名称一样的指令*/
     for (i = 0; cf->cycle->modules[i]; i++) {
 
         cmd = cf->cycle->modules[i]->commands;
