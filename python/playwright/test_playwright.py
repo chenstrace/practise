60a9f59be4151eae040de09d44c4ad8f5a197d0b
chenstrace <976338155@qq.com>
2023-09-26 19:57:04 +0800

    python/playwright/test_playwright.py

--- python/playwright/test_playwright.py
+++ python/playwright/test_playwright.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright
 # SELENIUM_REMOTE_URL=http://10.7.136.65:4444 python3 test.py
 
 
-def test_playwright(url="https://www.google.com", har_file="output.json"):
+def test_playwright(url="https://www.google.com", har_file=None):
 
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=True)
